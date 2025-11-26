3eeaaf6a2630610c30a49f8b35800bb238c258ad
ddeitterick <doug@deitterick.com>
2025-11-26 00:42:35 -0500

    Added additional fields to the query string

--- download_costco_receipts.js
+++ download_costco_receipts.js
@@ -6,104 +6,142 @@ async function listReceipts(startDate, endDate) {
         var xhr = new XMLHttpRequest();
         xhr.responseType = 'json';
         xhr.open('POST', 'https://ecom-api.costco.com/ebusiness/order/v1/orders/graphql');
-        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
         xhr.setRequestHeader('Content-Type', 'application/json-patch+json');
         xhr.setRequestHeader('Costco.Env', 'ecom');
         xhr.setRequestHeader('Costco.Service', 'restOrders');
         xhr.setRequestHeader('Costco-X-Wcs-Clientid', localStorage.getItem('clientID'));
         xhr.setRequestHeader('Client-Identifier', '481b1aec-aa3b-454b-b81b-48187e28f205');
         xhr.setRequestHeader('Costco-X-Authorization', 'Bearer ' + localStorage.getItem('idToken'));
-                        const listReceiptsQuery = {
-                                "query": `
-                                        query receiptsWithCounts($startDate: String!, $endDate: String!) {
-                                            receiptsWithCounts(startDate: $startDate, endDate: $endDate) {
-                                                inWarehouse
-                                                gasStation
-                                                carWash
-                                                gasAndCarWash
-                                                receipts {
-                                                    warehouseName
-                                                    receiptType
-                                                    documentType
-                                                    transactionDateTime
-                                                    transactionDate
-                                                    companyNumber
-                                                    warehouseNumber
-                                                    operatorNumber
-                                                    warehouseShortName
-                                                    registerNumber
-                                                    transactionNumber
-                                                    transactionType
-                                                    transactionBarcode
-                                                    total
-                                                    warehouseAddress1
-                                                    warehouseAddress2
-                                                    warehouseCity
-                                                    warehouseState
-                                                    warehouseCountry
-                                                    warehousePostalCode
-                                                    totalItemCount
-                                                    subTotal
-                                                    taxes
-                                                    instantSavings
-                                                    membershipNumber
-                                                    itemArray {
-                                                        itemNumber
-                                                        itemDescription01
-                                                        frenchItemDescription1
-                                                        itemDescription02
-                                                        frenchItemDescription2
-                                                        itemIdentifier
-                                                        unit
-                                                        amount
-                                                        taxFlag
-                                                        merchantID
-                                                        entryMethod
-                                                    }
-                                                    tenderArray {
-                                                        tenderTypeCode
-                                                        tenderDescription
-                                                        amountTender
-                                                        displayAccountNumber
-                                                        sequenceNumber
-                                                        approvalNumber
-                                                        responseCode
-                                                        transactionID
-                                                        merchantID
-                                                        entryMethod
-                                                    }
-                                                    couponArray {
-                                                        upcnumberCoupon
-                                                        voidflagCoupon
-                                                        refundflagCoupon
-                                                        taxflagCoupon
-                                                        amountCoupon
-                                                    }
-                                                    subTaxes {
-                                                        tax1
-                                                        tax2
-                                                        tax3
-                                                        tax4
-                                                        aTaxPercent
-                                                        aTaxLegend
-                                                        aTaxAmount
-                                                        bTaxPercent
-                                                        bTaxLegend
-                                                        bTaxAmount
-                                                        cTaxPercent
-                                                        cTaxLegend
-                                                        cTaxAmount
-                                                        dTaxAmount
-                                                    }
-                                                }
-                                            }
-                                        }`.replace(/\s+/g,' '),
-                                "variables": {
-                                        "startDate": startDate,
-                                        "endDate": endDate
-                                }
-                        };
-        xhr.onload = async function() {
+
+        const listReceiptsQuery = {
+            query: `
+                query receiptsWithCounts($startDate: String!, $endDate: String!) {
+                    receiptsWithCounts(startDate: $startDate, endDate: $endDate) {
+                        inWarehouse
+                        gasStation
+                        carWash
+                        gasAndCarWash
+                        receipts {
+                            warehouseName
+                            receiptType
+                            documentType
+                            transactionDateTime
+                            transactionDate
+                            companyNumber
+                            warehouseNumber
+                            operatorNumber
+                            warehouseShortName
+                            registerNumber
+                            transactionNumber
+                            transactionType
+                            transactionBarcode
+                            total
+                            warehouseAddress1
+                            warehouseAddress2
+                            warehouseCity
+                            warehouseState
+                            warehouseCountry
+                            warehousePostalCode
+                            totalItemCount
+                            subTotal
+                            taxes
+                            instantSavings
+                            membershipNumber
+                            invoiceNumber
+                            sequenceNumber
+                            itemArray {
+                                itemNumber
+                                itemDescription01
+                                frenchItemDescription1
+                                itemDescription02
+                                frenchItemDescription2
+                                itemIdentifier
+                                itemDepartmentNumber
+                                unit
+                                amount
+                                taxFlag
+                                merchantID
+                                entryMethod
+                                transDepartmentNumber
+                                fuelUnitQuantity
+                                fuelGradeCode
+                                itemUnitPriceAmount
+                                fuelUomCode
+                                fuelUomDescription
+                                fuelUomDescriptionFr
+                                fuelGradeDescription
+                                fuelGradeDescriptionFr
+                            }
+                            tenderArray {
+                                tenderTypeCode
+                                tenderSubTypeCode
+                                tenderDescription
+                                amountTender
+                                displayAccountNumber
+                                sequenceNumber
+                                approvalNumber
+                                responseCode
+                                transactionID
+                                merchantID
+                                entryMethod
+                                tenderTypeName
+                                tenderAcctTxnNumber
+                                tenderAuthorizationCode
+                                tenderTypeNameFr
+                                tenderEntryMethodDescription
+                                walletType
+                                walletId
+                                storedValueBucket
+                            }
+                            couponArray {
+                                upcnumberCoupon
+                                voidflagCoupon
+                                refundflagCoupon
+                                taxflagCoupon
+                                amountCoupon
+                            }
+                            subTaxes {
+                                tax1
+                                tax2
+                                tax3
+                                tax4
+                                aTaxPercent
+                                aTaxLegend
+                                aTaxAmount
+                                aTaxPrintCode
+                                aTaxPrintCodeFR
+                                aTaxIdentifierCode
+                                bTaxPercent
+                                bTaxLegend
+                                bTaxAmount
+                                bTaxPrintCode
+                                bTaxPrintCodeFR
+                                bTaxIdentifierCode
+                                cTaxPercent
+                                cTaxLegend
+                                cTaxAmount
+                                cTaxIdentifierCode
+                                dTaxPercent
+                                dTaxLegend
+                                dTaxAmount
+                                dTaxPrintCode
+                                dTaxPrintCodeFR
+                                dTaxIdentifierCode
+                                uTaxLegend
+                                uTaxAmount
+                                uTaxableAmount
+                            }
+                        }
+                    }
+                }`.replace(/\s+/g, ' '),
+            variables: {
+                startDate: startDate,
+                endDate: endDate,
+            },
+        };
+
+        xhr.onload = async function () {
             if (xhr.status === 200) {
                 try {
                     const data = xhr.response && xhr.response.data;
@@ -124,6 +162,7 @@ async function listReceipts(startDate, endDate) {
                 reject(xhr.status);
             }
         };
+
         xhr.send(JSON.stringify(listReceiptsQuery));
     });
 }
@@ -132,16 +171,16 @@ async function downloadReceipts() {
     var startDateStr = '01/01/2000';
     var endDate = new Date();
     var endDateStr = endDate.toLocaleDateString('en-US', {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit"
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
     });
     var receipts = await listReceipts(startDateStr, endDateStr);
-    console.log(`Got ${receipts.length} receipts, saving.`)
+    console.log(`Got ${receipts.length} receipts, saving.`);
     {
         var a = document.createElement('a');
-        a.download = `costco-${endDate.toISOString()}.json`
-        a.href = window.URL.createObjectURL(new Blob([JSON.stringify(receipts, null, 2)], {type: 'text/plain'}));
+        a.download = `costco-${endDate.toISOString()}.json`;
+        a.href = window.URL.createObjectURL(new Blob([JSON.stringify(receipts, null, 2)], { type: 'text/plain' }));
         a.target = '_blank';
         document.body.appendChild(a);
         a.click();
